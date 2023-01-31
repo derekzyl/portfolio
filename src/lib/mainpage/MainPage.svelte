d054dcabdaf75b51557e8c2dcaf52b094c8a927e
derek og <derekzyl@gmail.com>
2023-01-31 18:17:32 +0100

    did it its working now

---
+++ src/lib/mainpage/MainPage.svelte
@@ -0,0 +1,20 @@
+
+<script type="ts">
+  import { initOscillator } from "./logics/canvas";
+  import Name from "./plugins/Name.svelte";
+  
+
+
+
+
+
+window.addEventListener("load", () => {
+  console.log("page is fully loaded");
+  initOscillator(false);
+});
+</script>
+<div class="main-page" on:scroll={(e)=>console.log(window.scrollY)}>
+<Name/>
+  <canvas id="canvas"></canvas>
+
+</div>
---
+++ src/lib/mainpage/plugins/Name.svelte
@@ -0,0 +1,29 @@
+
+
+
+<script>
+    // your script goes here
+</script>
+<div class="fullname">
+
+
+<div class="firstname">
+    <div class="">d</div>    
+    <div class="">e</div>
+    <div class="">r</div>
+    <div class="">e</div>
+    <div class="">k</div>
+
+</div>
+
+<div class="lastname">
+    <div class="">o</div>
+    <div class="">g</div>
+    <div class="">a</div>
+    <div class="">g</div>
+    <div class="">a</div>
+    <div class="">r</div>
+    <div class="">u</div>
+    <div class="">e</div>
+</div>
+</div>
